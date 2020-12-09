438a8f01418a6a715c31929fb380aeaf09f8a869
Optimox <sebastien.fischman@gmail.com>
2020-12-09 10:38:59 +0100

    feat : jit ensemble prediction v1

--- code/training/predict.py
+++ code/training/predict.py
@@ -60,3 +60,46 @@ def predict_entire_mask(predict_dataset, model, batch_size = 32, threshold=None)
         global_pred = np.divide(global_pred, global_counter).astype(np.float16)
     
     return global_pred
+
+def predict_ensemble(predict_dataset, models_path, batch_size = 32, threshold=None, device='cuda'):
+    reduce_fac = predict_dataset.reduce_factor
+    predict_loader = DataLoader(predict_dataset, batch_size=batch_size, shuffle=False)
+
+    global_pred = np.zeros((predict_dataset.orig_size[0], predict_dataset.orig_size[1]), dtype=np.uint16)
+    global_counter = np.zeros((predict_dataset.orig_size[0], predict_dataset.orig_size[1]), dtype=np.uint16)
+
+    for model_path in models_path:
+        model = torch.jit.load(model_path).to(device)
+        # Make all predictions by batch
+        res = []
+        model.eval()
+        for batch in tqdm(predict_loader):
+            raw_preds = model(batch.to(device))
+            b_size, C, H, W = raw_preds.shape
+            upscale_preds = torch.nn.functional.interpolate(raw_preds,
+                                                            (H*reduce_fac, W*reduce_fac))
+            preds = torch.sigmoid(upscale_preds) > 0.5
+            preds = preds.detach().cpu().squeeze(dim=1).numpy().astype(np.uint8)
+            for b in range(preds.shape[0]):
+                res.append(sparse.csr_matrix(preds[b,:,:]))
+
+        for tile_idx, (pos_x, pos_y) in enumerate(predict_dataset.positions):
+            global_pred[pos_x[0]:pos_x[1],
+                            pos_y[0]:pos_y[1]] += res[tile_idx]#.toarray()
+            global_counter[pos_x[0]:pos_x[1],
+                        pos_y[0]:pos_y[1]] += 1
+        del res
+        del model
+    del predict_loader
+
+    # divide by overlapping tiles
+    if threshold is not None:
+        bool_pred = np.zeros((predict_dataset.orig_size[0], predict_dataset.orig_size[1]), dtype=np.bool)
+        for row in tqdm(range(predict_dataset.orig_size[0])):
+            bool_pred[row, :] = (np.divide(global_pred[row, :],
+                                            global_counter[row, :]) > threshold).astype(np.bool)
+        return bool_pred
+    else:
+        global_pred = np.divide(global_pred, global_counter).astype(np.float16)
+    
+    return global_pred
